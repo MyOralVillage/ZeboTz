e0a1c9948e573176a9b94777f2a7c8cdd13cb606
Kamilla Karthigesu <kamillak@outlook.com>
2015-11-08 18:49:21 -0500

    Added question images
    will always be tens- so far no hundreds or thousands

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/GamePlaceValueActivity.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/GamePlaceValueActivity.java
@@ -2,16 +2,25 @@ package com.fydp.myoralvillage;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 
 public class GamePlaceValueActivity extends ActionBarActivity {
-
+    public int difficultyLevel = 1;
+    public int correctAnswer=0;
+    //1: beginner (tens, hundreds, or thousands)
+    //2: intermediate (tens, hundreds, and thousands)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_place_value);
+        startNewRoundGamePlaceValue();
     }
 
     @Override
@@ -34,5 +43,65 @@ public class GamePlaceValueActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void resetGame(View v) {
+        startNewRoundGamePlaceValue();
+    }
+
+    public void startNewRoundGamePlaceValue () {
+        correctAnswer = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            String imgView_name = "img_representation"+i;
+            int res_id = getResources().getIdentifier(imgView_name, "id", getPackageName());
+            ImageView iv = (ImageView) findViewById(res_id);
+            iv.setVisibility(View.INVISIBLE);
+        }
+
+        generateRepresentation();
+    }
+
+    public void generateRepresentation() {
+        if(difficultyLevel==1){
+            //maximum 9 images (90, 900, 9000)
+            Random randomPlaceValue = new Random();
+            //int pv=randomPlaceValue.nextInt(3)+1;
+            int pv = 1;
+            if (pv==1){
+                Random r = new Random();
+                correctAnswer = (r.nextInt(9)+1)*10;
+            }
+            int numRepresentationImages=getNumRepresentationImages();
+            segmentRepresentation(numRepresentationImages);
+        } else {
+            //maximum 27 images (9990)- we'll deal with this later
+        }
+    }
+
+    public int getNumRepresentationImages() {
+        return (int)correctAnswer/10;
+    }
+    public void segmentRepresentation(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            int img_id = getResources().getIdentifier("gameplacevalue_representation10", "drawable", getPackageName());
+            String imgView_name = "img_representation"+i;
+            int res_id = getResources().getIdentifier(imgView_name, "id", getPackageName());
+            ImageView iv = (ImageView) findViewById(res_id);
+
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+            int screenHeight = metrics.heightPixels;
+            int screenWidth = metrics.widthPixels;
+
+            iv.requestLayout();
+            iv.getLayoutParams().height = (int)(screenHeight*0.2);
+            iv.getLayoutParams().width = (int)(screenWidth/(n+1));
+            iv.setImageResource(img_id);
+            iv.setTag("gameplacevalue_representation10");
+            iv.setVisibility(View.VISIBLE);
+        }
     }
 }
