3b2a9231c55bc0904d37ad59c8080394b4645eb0
Kamilla Karthigesu <kamillak@outlook.com>
2016-01-11 17:11:23 -0500

    Put PlaceValue into a Level 2 Activity
    Added baskets of tomatoes, resizable

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level2Activity.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level2Activity.java
@@ -1,25 +1,28 @@
 package com.fydp.myoralvillage;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-
-public class GameMenuActivity extends ActionBarActivity {
+public class Level2Activity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_menu);
+        setContentView(R.layout.activity_level2);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_game_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_game2, menu);
         return true;
     }
 
@@ -38,23 +41,9 @@ public class GameMenuActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void goToGame1(View v) {
-        Intent intent = new Intent(this, Game1Activity.class);
-        startActivity(intent);
-    }
-
-    public void goToGame2(View v) {
-        Intent intent = new Intent(this, Game2Activity.class);
-        startActivity(intent);
-    }
-
-    public void goToLevel2(View v) {
-        Intent intent = new Intent(this, Level2Activity.class);
-        startActivity(intent);
-    }
-
-    public void goToGamePlaceValue(View v) {
+    public void goToLevel2PlaceValue(View v) {
         Intent intent = new Intent(this, GamePlaceValueActivity.class);
         startActivity(intent);
     }
+
 }
