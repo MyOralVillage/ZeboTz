52de7de12687eb9849fb034dc1faafe4b8fee475
Kamilla Karthigesu <kamillak@outlook.com>
2016-01-20 20:41:23 -0500

    Merging Usama's tracing

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
@@ -2,28 +2,42 @@ package com.fydp.myoralvillage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Level1ActivityGameTracing extends AppCompatActivity {
-    //getValue for userHasViewedDemo from text file, set it here
-    //right now, because there are no users, we'll set this to false
-    //(user must view demo every time)
-    public boolean userHasViewedDemo = false;
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
+
+
+    private PaintView myView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1_gametracing);
 
-        if(!userHasViewedDemo) {
-            startDemo();
-        }
         startGame();
     }
 
-    public void startDemo() {
-        //go to Demo Activity (a separate activity)
-    }
 
     public void startGame() {
         //game code in here as well as game method calls
+        myView = (PaintView)findViewById(R.id.custView);
+    }
+
+    public void newNumber(View v) {
+        final Random rand = new Random();
+
+        int randNum = rand.nextInt(10);
+        myView.setNumber(randNum);
     }
 }
