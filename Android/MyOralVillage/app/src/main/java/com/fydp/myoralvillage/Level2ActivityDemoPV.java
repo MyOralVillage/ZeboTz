02eef6066e773342daca1ae538832de37a87d287
Kamilla Karthigesu <kamillak@outlook.com>
2016-02-15 15:15:22 -0500

    First half of demo complete

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level2ActivityDemoPV.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level2ActivityDemoPV.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Level2ActivityDemoPV extends AppCompatActivity {
-    int currentNumber = 0;
+    int demoStage = 0;
+    int demoType = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,34 +23,44 @@ public class Level2ActivityDemoPV extends AppCompatActivity {
     }
 
     public void nextNumber(View v) {
-
-        if (currentNumber==9) {
-            finish();
-        } else {
-            currentNumber++;
-            String numberFileName = "game1_demo_dualcoding_" + currentNumber;
-            String representationFileName = "game1_dualcoding_" + currentNumber;
-
-            int img_id_number = getResources().getIdentifier(numberFileName, "drawable", getPackageName());
-            int img_id_representation = getResources().getIdentifier(representationFileName, "drawable", getPackageName());
-
-            ImageView ivNumber = (ImageView) findViewById(R.id.img_number);
-            ImageView ivRepresentation = (ImageView) findViewById(R.id.img_representation);
-
-            ivNumber.setImageResource(img_id_number);
-            ivRepresentation.setImageResource(img_id_representation);
-            if(currentNumber!=0) {
-                ImageView iv = (ImageView) findViewById(R.id.btn_lvl1_dualCoding_previous);
-                iv.setVisibility(iv.VISIBLE);
+        if(demoType==0) {
+            if (demoStage==2) {
+                finish();
             } else {
-                ImageView iv = (ImageView) findViewById(R.id.btn_lvl1_dualCoding_previous);
-                iv.setVisibility(iv.INVISIBLE);
+                demoStage++;
+                String representationFileName;
+                String numeral;
+                if(demoStage==1) {
+                    representationFileName = "game2_tens";
+                    numeral="10";
+                } else if (demoStage == 2) {
+                    representationFileName = "game2_hundreds";
+                    numeral="100";
+                } else {
+                    representationFileName = "game2_ones";
+                    numeral="1";
+                }
+
+                int img_id_representation = getResources().getIdentifier(representationFileName, "drawable", getPackageName());
+
+                ImageView ivRepresentation = (ImageView) findViewById(R.id.img_representation);
+                TextView tvNumber = (TextView) findViewById(R.id.tv_2);
+
+                ivRepresentation.setImageResource(img_id_representation);
+                tvNumber.setText(numeral);
+                if(demoStage!=0) {
+                    ImageView iv = (ImageView) findViewById(R.id.btn_lvl1_dualCoding_previous);
+                    iv.setVisibility(iv.VISIBLE);
+                } else {
+                    ImageView iv = (ImageView) findViewById(R.id.btn_lvl1_dualCoding_previous);
+                    iv.setVisibility(iv.INVISIBLE);
+                }
             }
         }
     }
 
     public void prevNumber(View v) {
-        if (currentNumber==1) {
+        if (demoStage==1) {
             ImageView iv = (ImageView) findViewById(R.id.btn_lvl1_dualCoding_previous);
             iv.setVisibility(iv.INVISIBLE);
         } else {
@@ -54,17 +68,26 @@ public class Level2ActivityDemoPV extends AppCompatActivity {
             ImageView iv = (ImageView) findViewById(R.id.btn_lvl1_dualCoding_previous);
             iv.setVisibility(iv.VISIBLE);
         }
-        currentNumber--;
-        String numberFileName = "game1_demo_dualcoding_" + currentNumber;
-        String representationFileName = "game1_dualcoding_" + currentNumber;
+        demoStage--;
+        String representationFileName;
+        String numeral;
+        if(demoStage==1) {
+            representationFileName = "game2_tens";
+            numeral = "10";
+        } else if (demoStage == 2) {
+            representationFileName = "game2_hundreds";
+            numeral = "100";
+        } else {
+            representationFileName = "game2_ones";
+            numeral = "1";
+        }
 
-        int img_id_number = getResources().getIdentifier(numberFileName, "drawable", getPackageName());
         int img_id_representation = getResources().getIdentifier(representationFileName, "drawable", getPackageName());
 
-        ImageView ivNumber = (ImageView) findViewById(R.id.img_number);
         ImageView ivRepresentation = (ImageView) findViewById(R.id.img_representation);
+        TextView tvNumber = (TextView) findViewById(R.id.tv_2);
 
-        ivNumber.setImageResource(img_id_number);
+        tvNumber.setText(numeral);
         ivRepresentation.setImageResource(img_id_representation);
     }
 }
