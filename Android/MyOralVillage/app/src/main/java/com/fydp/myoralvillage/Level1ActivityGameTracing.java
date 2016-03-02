639c621dce36745cc8fb3d6d077ea63a28421f9e
Kamilla-K <kamillak@outlook.com>
2016-03-02 17:56:33 -0500

    update usersettings.txt from L1 Tracing

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
@@ -1,5 +1,7 @@
  package com.fydp.myoralvillage;
 
+        import android.content.Intent;
+        import android.os.Environment;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.MotionEvent;
@@ -8,6 +10,10 @@
         import android.widget.ImageView;
         import android.widget.TextView;
 
+        import java.io.BufferedReader;
+        import java.io.File;
+        import java.io.FileOutputStream;
+        import java.io.FileReader;
         import java.util.Random;
 
 public class Level1ActivityGameTracing extends AppCompatActivity {
@@ -15,14 +21,27 @@ public class Level1ActivityGameTracing extends AppCompatActivity {
     private ImageView hImageViewPic;
     int[] images = { R.drawable.game1_tracing_0, R.drawable.game1_tracing_1, R.drawable.game1_tracing_2, R.drawable.game1_tracing_3, R.drawable.game1_tracing_4, R.drawable.game1_tracing_5, R.drawable.game1_tracing_6, R.drawable.game1_tracing_7, R.drawable.game1_tracing_8, R.drawable.game1_tracing_9 };
 
+    public UserSettings thisUser = new UserSettings();
+    File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level1_gametracing);
 
+        Intent intent = getIntent();
+        getExtras(intent);
+
         startGame();
     }
 
+    public void getExtras(Intent intent) {
+        thisUser.userName = intent.getStringExtra("USERSETTINGS_USERNAME");
+        thisUser.userId = intent.getIntExtra("USERSETTINGS_USERID", -1);
+        thisUser.demosViewed = intent.getBooleanArrayExtra("USERSETTINGS_DEMOSVIEWED");
+        thisUser.availableLevels = intent.getBooleanArrayExtra("USERSETTINGS_AVAILABLELEVELS");
+        thisUser.activityProgress = intent.getBooleanArrayExtra("USERSETTINGS_ACTIVITYPROGRESS");
+    }
 
     public void startGame() {
         myView = (PaintView)findViewById(R.id.custView);
@@ -41,5 +60,70 @@ public class Level1ActivityGameTracing extends AppCompatActivity {
 
         hImageViewPic.setImageResource(images[randNum]);
         myView.setNumber(randNum);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(!thisUser.userName.equals("admin")) {
+            thisUser.activityProgress[1] = true;
+            updateUserSettings();
+        }
+    }
+
+    public String stringifyUserSetting() {
+        String thisString = thisUser.userName + "," + String.valueOf(thisUser.userId);
+        for(int i = 0; i < thisUser.demosViewed.length; i++) {
+            thisString += "," + String.valueOf(thisUser.demosViewed[i]);
+        }
+        for(int i = 0; i < thisUser.availableLevels.length; i++) {
+            thisString += "," + String.valueOf(thisUser.availableLevels[i]);
+        }
+        for(int i = 0; i < thisUser.activityProgress.length; i++) {
+            thisString += "," + String.valueOf(thisUser.activityProgress[i]);
+        }
+
+        return thisString;
+    }
+
+    public void updateUserSettings() {
+        File userSettingsFile = new File(root, "usersettings.txt");
+
+        try {
+            // input the file content to the String "input"
+            BufferedReader file = new BufferedReader(new FileReader(userSettingsFile));
+            String line;
+            String input = "";
+            String newLine ="";
+            String oldLine ="";
+
+            while ((line = file.readLine()) != null) {
+                String[] thisLine = line.split(",");
+                if(thisLine[0].equals(thisUser.userName)) {
+                    newLine = stringifyUserSetting();
+                    oldLine = line;
+                }
+                input += line + '\n';
+            }
+
+            file.close();
+
+            if(!oldLine.equals(newLine)) {
+                input = input.replace(oldLine, newLine);
+            }
+            // write the new String with the replaced line OVER the same file
+            FileOutputStream fileOut = new FileOutputStream(userSettingsFile);
+            fileOut.write(input.getBytes());
+            fileOut.close();
+
+        } catch (Exception e) {
+            System.out.println("Problem reading file.");
+        }
     }
 }
