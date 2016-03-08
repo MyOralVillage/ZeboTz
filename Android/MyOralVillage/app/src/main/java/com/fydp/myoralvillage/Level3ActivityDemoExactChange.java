6532d31f3c22182ef57c56ee6b9d87cfeacec5f1
Usama Ahmed <Usama Ahmed>
2016-03-07 20:26:15 -0500

    exact change and currenct demos now have one finger appearing instead of multiple

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level3ActivityDemoExactChange.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level3ActivityDemoExactChange.java
@@ -1,15 +1,23 @@
 package com.fydp.myoralvillage;
 
+import android.graphics.Point;
 import android.graphics.Typeface;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.TypedValue;
+import android.view.Display;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import android.os.Handler;
 
 /**
  * Created by Usama on 3/2/2016.
@@ -25,9 +33,23 @@ public class Level3ActivityDemoExactChange extends ActionBarActivity {
     public void startDemo(){
 
         ImageButton mSkip = (ImageButton) findViewById(R.id.skip_button);
+        ImageView imagefinger;
+        final RelativeLayout rl = (RelativeLayout) findViewById(R.id.myrellayout);
         final ImageView finger1 = (ImageView) findViewById(R.id.finger1);
-        final ImageView finger3 = (ImageView) findViewById(R.id.finger3);
-        final ImageView finger3anim = (ImageView) findViewById(R.id.finger3);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(60, 60);
+        params.leftMargin = 30;
+        params.topMargin = 350;
+        rl.removeView(finger1);
+        rl.addView(finger1, params);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int maxX = size.x;
+        int maxY = size.y;
+
+        System.out.println(maxX + "and " + maxY);
+
 
 
         int x = finger1.getLeft();
@@ -74,6 +96,7 @@ public class Level3ActivityDemoExactChange extends ActionBarActivity {
 
 
         firstAnimationSet.addAnimation(animation1);
+
         finger1.startAnimation(firstAnimationSet);
         bill500.startAnimation(firstAnimationSet);
         firstAnimationSet.setAnimationListener(new Animation.AnimationListener() {
@@ -81,7 +104,7 @@ public class Level3ActivityDemoExactChange extends ActionBarActivity {
             @Override
             public void onAnimationStart(Animation animation) {
                 // TODO Auto-generated method stub
-
+                finger1.setVisibility(View.VISIBLE);
             }
 
             @Override
@@ -107,10 +130,74 @@ public class Level3ActivityDemoExactChange extends ActionBarActivity {
             }
         });
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                //final ImageView fingerX = (ImageView) findViewById(R.id.finger1);
+                final ImageView imagefinger = new ImageView(Level3ActivityDemoExactChange.this);
+                imagefinger.setBackgroundResource(R.drawable.finger);
+                int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 60, getResources().getDisplayMetrics());
+                RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(30, 50);
+                params2.leftMargin = 400;
+                params2.topMargin = 350;
+                rl.addView(imagefinger, params2);
+
+
+                // first animation/drag action
+                final AnimationSet secondAnimationSet = new AnimationSet(true);
+
+                TranslateAnimation animation2 = new TranslateAnimation(-10, -10,
+                        5, 42);          //  new TranslateAnimation(xFrom,xTo, yFrom,yTo)
+                animation2.setStartOffset(500);
+                animation2.setDuration(3000);  // animation duration
+                animation2.setRepeatCount(0);  // animation repeat count
+                animation2.setRepeatMode(1);   // repeat animation (left to right, right to left )
+                //      animation.setFillAfter(true);
+
+                //img_animation.startAnimation(animation);  // start animation
+                secondAnimationSet.addAnimation(animation2);
+
+                TranslateAnimation animation22 = new TranslateAnimation(5, 5,
+                        0, -150);          //  new TranslateAnimation(xFrom,xTo, yFrom,yTo)
+                animation22.setStartOffset(500);
+                animation22.setDuration(3000);  // animation duration
+                animation22.setRepeatCount(0);  // animation repeat count
+                animation22.setRepeatMode(1);   // repeat animation (left to right, right to left )
+                animation22.setFillAfter(true);
+                animation22.setFillEnabled(true);
+
+                secondAnimationSet.addAnimation(animation22);
+                imagefinger.startAnimation(secondAnimationSet);
+                bill2000.startAnimation(secondAnimationSet);
+                secondAnimationSet.setAnimationListener(new Animation.AnimationListener() {
+
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // TODO Auto-generated method stub
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // TODO Auto-generated method stub
+                        //img_animation1.layout(1500,400,1500,1000);
+                        bill2000Snap.setBackgroundResource(R.drawable.bill_2000);
+                        imagefinger.setVisibility(View.INVISIBLE);
+                        cashView.setText("2500");
+                    }
+                });
+            }
+        }, 3400);
 
 
 
-
+/**
         // first animation/drag action
         final AnimationSet secondAnimationSet = new AnimationSet(true);
 
@@ -134,10 +221,8 @@ public class Level3ActivityDemoExactChange extends ActionBarActivity {
         animation22.setFillAfter(true);
         animation22.setFillEnabled(true);
 
-        finger3anim.setVisibility(View.VISIBLE);
-
         secondAnimationSet.addAnimation(animation22);
-        finger3.startAnimation(secondAnimationSet);
+        imagefinger.startAnimation(secondAnimationSet);
         bill2000.startAnimation(secondAnimationSet);
         secondAnimationSet.setAnimationListener(new Animation.AnimationListener() {
 
@@ -158,11 +243,11 @@ public class Level3ActivityDemoExactChange extends ActionBarActivity {
                 // TODO Auto-generated method stub
                 //img_animation1.layout(1500,400,1500,1000);
                 bill2000Snap.setBackgroundResource(R.drawable.bill_2000);
-                finger3.setVisibility(View.INVISIBLE);
+                finger2.setVisibility(View.INVISIBLE);
                 cashView.setText("2500");
             }
         });
-
+**/
         mSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
