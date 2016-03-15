ce99da4737506a45c92c954ee57b1f7c7b1e093e
Usama Ahmed <Usama Ahmed>
2016-03-15 15:43:42 -0400

    tracing game logic correction

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
@@ -15,12 +15,14 @@
         import java.io.FileOutputStream;
         import java.io.FileReader;
         import java.util.Random;
+        import android.os.Handler;
 
 public class Level1ActivityGameTracing extends AppCompatActivity {
     private PaintView myView;
     private ImageView hImageViewPic;
     int[] images = { R.drawable.game1_tracing_0, R.drawable.game1_tracing_1, R.drawable.game1_tracing_2, R.drawable.game1_tracing_3, R.drawable.game1_tracing_4, R.drawable.game1_tracing_5, R.drawable.game1_tracing_6, R.drawable.game1_tracing_7, R.drawable.game1_tracing_8, R.drawable.game1_tracing_9 };
     public int numCorrect=0;
+    public int questionNumber=0;
     public UserSettings thisUser = new UserSettings();
     File root = new File(Environment.getExternalStorageDirectory(), "Notes");
 
@@ -50,28 +52,39 @@ public class Level1ActivityGameTracing extends AppCompatActivity {
         myView = (PaintView)findViewById(R.id.custView);
         hImageViewPic = (ImageView)findViewById(R.id.image_view);
 
-        final Random rand = new Random();
-        int randNum = rand.nextInt(10);
+        //final Random rand = new Random();
+        //int randNum = rand.nextInt(10);
 
-        hImageViewPic.setImageResource(images[randNum]);
-        myView.setNumber(randNum);
+        hImageViewPic.setImageResource(images[questionNumber]);
+        myView.setNumber(questionNumber);
+        questionNumber++;
+        numCorrect++;
     }
 
     public void newNumber(View v) {
-        final Random rand = new Random();
-        int randNum = rand.nextInt(10);
-
-        numCorrect++;
+        //final Random rand = new Random();
+        //int randNum = rand.nextInt(10);
+        if(numCorrect==10) {
+            finish();
+        }
         String score_name = "star"+numCorrect;
         int score_id = getResources().getIdentifier(score_name, "drawable", getPackageName());
         ImageView tv = (ImageView) findViewById(R.id.score);
         tv.setImageResource(score_id);
-
-        hImageViewPic.setImageResource(images[randNum]);
-        myView.setNumber(randNum);
-        if(numCorrect==10) {
-            finish();
+        if (questionNumber != 10) {
+            hImageViewPic.setImageResource(images[questionNumber]);
         }
+        else{
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    finish();
+                }
+            }, 1000);
+        }
+        myView.setNumber(questionNumber);
+        questionNumber++;
+        numCorrect++;
     }
 
 
