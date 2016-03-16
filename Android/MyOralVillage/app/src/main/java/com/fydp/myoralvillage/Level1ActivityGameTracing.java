d4f77678f08c69ba82f2b3a6d56b1a0bf91d470c
Usama Ahmed <Usama Ahmed>
2016-03-15 20:56:28 -0400

    tracing logic improvement

--- Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
+++ Android/MyOralVillage/app/src/main/java/com/fydp/myoralvillage/Level1ActivityGameTracing.java
@@ -11,6 +11,7 @@
         import android.widget.Button;
         import android.widget.ImageView;
         import android.widget.TextView;
+        import android.os.Handler;
 
         import java.io.BufferedReader;
         import java.io.File;
@@ -23,6 +24,7 @@ public class Level1ActivityGameTracing extends AppCompatActivity {
     private ImageView hImageViewPic;
     int[] images = { R.drawable.game1_tracing_0, R.drawable.game1_tracing_1, R.drawable.game1_tracing_2, R.drawable.game1_tracing_3, R.drawable.game1_tracing_4, R.drawable.game1_tracing_5, R.drawable.game1_tracing_6, R.drawable.game1_tracing_7, R.drawable.game1_tracing_8, R.drawable.game1_tracing_9 };
     public int numCorrect=0;
+    public int questionNumber=0;
     public UserSettings thisUser = new UserSettings();
     File root = new File(Environment.getExternalStorageDirectory(), "Notes");
 
@@ -52,29 +54,45 @@ public class Level1ActivityGameTracing extends AppCompatActivity {
         myView = (PaintView)findViewById(R.id.custView);
         hImageViewPic = (ImageView)findViewById(R.id.image_view);
 
-        final Random rand = new Random();
-        int randNum = rand.nextInt(10);
+        //final Random rand = new Random();
+        //int randNum = rand.nextInt(10);
 
-        hImageViewPic.setImageResource(images[randNum]);
-        myView.setNumber(randNum);
+        //hImageViewPic.setImageResource(images[randNum]);
+        //myView.setNumber(randNum);
+
+        hImageViewPic.setImageResource(images[questionNumber]);
+        myView.setNumber(questionNumber);
+        questionNumber++;
+        numCorrect++;
     }
 
     public void newNumber(View v) {
-        final Random rand = new Random();
-        int randNum = rand.nextInt(10);
-
-        numCorrect++;
-        String score_name = "star"+numCorrect;
-        int score_id = getResources().getIdentifier(score_name, "drawable", getPackageName());
-        ImageView tv = (ImageView) findViewById(R.id.score);
-        tv.setImageResource(score_id);
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.applause);
-        mediaPlayer.start();
-        if (numCorrect == 10) {
-            onBackPressed();
+        if(myView.completedNumber == true) {
+            MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.applause);
+            mediaPlayer.start();
+            //final Random rand = new Random();
+            //int randNum = rand.nextInt(10);
+            if (numCorrect == 10) {
+                onBackPressed();
+            }
+            String score_name = "star" + numCorrect;
+            int score_id = getResources().getIdentifier(score_name, "drawable", getPackageName());
+            ImageView tv = (ImageView) findViewById(R.id.score);
+            tv.setImageResource(score_id);
+            if (questionNumber != 10) {
+                hImageViewPic.setImageResource(images[questionNumber]);
+            } else {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        onBackPressed();
+                    }
+                }, 1000);
+            }
+            myView.setNumber(questionNumber);
+            questionNumber++;
+            numCorrect++;
         }
-        hImageViewPic.setImageResource(images[randNum]);
-        myView.setNumber(randNum);
     }
 
 
